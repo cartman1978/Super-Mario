9c031a6a4254c36273a72d7f9766b579a91a6e98
cartman1978 <cartman_7802@yahoo.ie>
2021-02-25 17:03:59 +0000

    added nav bar toggler class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
-$(window).scroll(function () {
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 200);
-});
+// $(window).scroll(function () {
+//     $('nav').toggleClass('scrolled', $(this).scrollTop() > 200);
+// });
+
+
